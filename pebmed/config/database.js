1ede1515c34944494045a54d7e4e04caa880904f
igormelo <igorak4@hotmail.com>
2021-07-27 13:44:44 -0300

    Removendo arquivos desnecessarios

---
+++ pebmed/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: '123456',
+    port: 3306,
+    database: 'pebmed',
+    define: {
+        timestamp: true,
+        underscored: true
+    },
+    timezone: '-03:00'
+}
